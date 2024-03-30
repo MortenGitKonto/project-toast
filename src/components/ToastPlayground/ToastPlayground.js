2c4f36a2e5145b60a353fd457978e16b7937a282
Morten <morten.jogi@gmail.com>
2024-03-30 13:18:03 +0100

    Exercise 2: Live-editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,18 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast/Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [toastHidden, setToastHidden] = React.useState(true);
+
+  const handleDismiss = () => {
+    setToastHidden(true);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +22,12 @@ function ToastPlayground() {
         <img alt='Cute toast mascot' src='/toast.png' />
         <h1>Toast Playground</h1>
       </header>
+
+      {!toastHidden && (
+        <Toast handleDismiss={handleDismiss} variant={variant}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -62,9 +74,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button onClick={() => alert(`${variant}: ${message}`)}>
-              Pop Toast!
-            </Button>
+            <Button onClick={() => setToastHidden(false)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
